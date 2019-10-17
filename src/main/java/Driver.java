788ddbb7111d9619e219ae982a252e425357aff6
Artem Golovin <artgolovin96@gmail.com>
2019-10-17 10:45:53 -0600

    Add option to output inspection results in std out

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,43 +4,68 @@ import java.io.*;
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        boolean rec = true;
-        if (args.length == 1) {
-            rec = Boolean.parseBoolean(args[1]);
+        boolean isRecursive = args[0] != null && Boolean.parseBoolean(args[0]);
+        boolean useStdOut = args[1] != null && Boolean.parseBoolean(args[1]);
+
+        runTest("script1.txt", new ClassA(), isRecursive, useStdOut);
+        runTest("script2.txt", new ClassA(12), isRecursive, useStdOut);
+        runTest("script3.txt", new ClassB(), isRecursive, useStdOut);
+        runTest("script4.txt", new ClassD(32), isRecursive, useStdOut);
+        runTest("script5.txt", new ClassD(), isRecursive, useStdOut);
+        runTest("script6.txt", new ClassB[12], isRecursive, useStdOut);
+        runTest("script7.txt", new ClassB[12][12], isRecursive, useStdOut);
+        runTest("script8.txt", "Test String", isRecursive, useStdOut);
+    }
+
+    private static void runTest(String filename, Object testObj, boolean recursive, boolean useStdOut) {
+        if (!useStdOut) {
+            runTest(filename, testObj, recursive);
+            return;
         }
-        runTest("script1.txt", new ClassA(), rec);
-        runTest("script2.txt", new ClassA(12), rec);
-        runTest("script3.txt", new ClassB(), rec);
-        runTest("script4.txt", new ClassD(32), rec);
-        runTest("script5.txt", new ClassD(), rec);
-        runTest("script6.txt", new ClassB[12], rec);
-        runTest("script7.txt", new ClassB[12][12], rec);
-        runTest("script8.txt", "Test String", rec);
+
+        try {
+            System.out.println();
+            runInspect(null, testObj, recursive);
+            System.out.println();
+
+        } catch (Exception e) {
+            System.err.println("Unable to compleatly run test: " + testObj);
+            e.printStackTrace();
+        }
     }
 
     public static void runTest(String filename, Object testObj, boolean recursive) {
         try {
-            PrintStream old = System.out;
+            PrintStream stdOut = System.out;
             File file = new File(filename);
-            FileOutputStream fos = new FileOutputStream(file);
-            PrintStream ps = new PrintStream(fos);
-            System.setOut(ps);
-            System.out.println("======================================================");
-            System.out.println("Filename: " + filename);
-            System.out.println("Running Test: " + testObj);
-            System.out.println("Recursive: " + recursive);
-            new Inspector().inspect(testObj, recursive);
-            System.out.println("======================================================");
-            ps.flush();
-            fos.flush();
-            ps.close();
-            fos.close();
-            System.setOut(old);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            PrintStream filePrintStream = new PrintStream(fileOutputStream);
+
+            System.setOut(filePrintStream);
+            runInspect(filename, testObj, recursive);
+
+            filePrintStream.flush();
+            fileOutputStream.flush();
+            filePrintStream.close();
+            fileOutputStream.close();
+
+            System.setOut(stdOut);
         } catch (IOException ioe) {
             System.err.println("Unable to open file: " + filename);
         } catch (Exception e) {
             System.err.println("Unable to compleatly run test: " + testObj);
             e.printStackTrace();
         }
+    }
+
+    private static void runInspect(String filename, Object testObj, boolean recursive) {
+        System.out.println("======================================================");
+        if (filename != null) {
+            System.out.println("Filename: " + filename);
+        }
+        System.out.println("Running Test: " + testObj);
+        System.out.println("Recursive: " + recursive);
+        new Inspector().inspect(testObj, recursive);
+        System.out.println("======================================================");
     }
 }
